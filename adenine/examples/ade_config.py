cf31076bdc7233ae900ee1141b694dc5505b4e1b
Samuele Fiorini <samuele.fiorini@gmail.com>
2016-04-05 17:54:04 +0200

    Now ade_run evaluates each pipeline in parallel using multiprocessing

--- adenine/examples/ade_config.py
+++ adenine/examples/ade_config.py
@@ -6,20 +6,19 @@ from adenine.utils import data_source
 # --------------------------  EXPERMIENT INFO ------------------------- #
 exp_tag = 'debug_csv'
 output_root_folder = 'results'
-# parallel = False
 
 # ----------------------------  INPUT DATA ---------------------------- #
 # X, y, feat_names, class_names = data_source.load('iris')
-# X, y, feat_names, class_names = data_source.load('gauss')
+X, y, feat_names, class_names = data_source.load('gauss')
 # X, y, feat_names, class_names = data_source.load('digits')
 # X, y, feat_names, class_names = data_source.load('diabetes')
 # X, y, feat_names, class_names = data_source.load('boston')
 # X, y, feat_names, class_names = data_source.load('custom', 'X.npy', 'y.npy')
 # X, y, feat_names, class_names = data_source.load('custom', 'X.csv', 'y.csv')
-X, y, feat_names, class_names = data_source.load('custom', '/home/fede/src/adenine/adenine/examples/TM_matrix.csv')
-if not (X.T == X).all():
-    X = (X.T + X) / 2.0
-    X = 1. - X
+# X, y, feat_names, class_names = data_source.load('custom', '/home/fede/src/adenine/adenine/examples/TM_matrix.csv')
+# if not (X.T == X).all():
+#     X = (X.T + X) / 2.0
+#     X = 1. - X
 
 # -----------------------  PIPELINE DEFINITION ------------------------ #
 
@@ -28,19 +27,19 @@ step0 = {'Impute': [False], 'Missing': [-1], 'Replacement': ['median','mean']}
 
 # --- Data Preprocessing --- #
 step1 = {'None': [True], 'Recenter': [False], 'Standardize': [False],
-         'Normalize': [False, ['l2']], 'MinMax': [False, [0,1]]}
+         'Normalize': [True, ['l2']], 'MinMax': [False, [0,1]]}
 
 # --- Dimensionality Reduction & Manifold Learning --- #
 step2 = {'PCA': [False], 'IncrementalPCA': [False], 'RandomizedPCA': [False],
-         'KernelPCA': [False, ['linear','rbf','poly']], 'Isomap': [False],
+         'KernelPCA': [True, ['linear','rbf','poly']], 'Isomap': [False],
          'LLE': [False, ['standard','modified','hessian', 'ltsa']],
-         'SE': [False], 'MDS': [False, ['metric','nonmetric']],
-         'tSNE': [False], 'None': [True]}
+         'SE': [True], 'MDS': [False, ['metric','nonmetric']],
+         'tSNE': [True], 'None': [True]}
 
 # --- Clustering --- #
-step3 = {'KMeans': [False, [5]],
-         'AP': [False, ['precomputed']], 'MS': [False],
-         'Spectral': [True, [50, ['precomputed']]],
+step3 = {'KMeans': [True, [3]],
+         'AP': [True], 'MS': [True],
+         'Spectral': [False, [50]],
          #'Hierarchical': [False, [3, ['manhattan','euclidean'], ['ward','complete','average']]]
-         'Hierarchical': [False, [3, ['precomputed']]]
+         'Hierarchical': [False, [3]]
          }
