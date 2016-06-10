4017e8bd3d2148ad3fdc1f748db3817057529c52
Samuele Fiorini <samuele.fiorini@gmail.com>
2016-06-10 16:27:14 +0200

    major paper review

--- JMLR_paper/experiments/pygm_config.py
+++ JMLR_paper/experiments/pygm_config.py
@@ -1,6 +1,6 @@
 from adenine.utils import data_source
-X, y, feats, classes = data_source.load('custom', 'data.csv', 'labels.csv')
-step1 = {'Normalize': [True, {'norm': 'l2'}]} # Preprocessing
-step2 = {'KernelPCA': [True, {'kernel': ['rbf'], 'n_components': 3, 'gamma':
-2}], 'Isomap': [True, {'n_components': 3}]} # Dimensionality reduction
-step3 = {'KMeans': [True, {'n_clusters': ['auto']}]} # Clustering
+X,y,feat_names,class_names = data_source.load("custom","data.csv","labels.csv")
+step1 = {'Normalize':[True,{'norm':'l2'}]} #Preprocessing
+step2 = {'PCA':[True,{'n_components':2}],'KernelPCA':[True,{'kernel': ['rbf'], \
+'n_components':3,'gamma':2}],'Isomap':[True,{'n_components':3}]} #Dim. Reduction
+step3 = {'KMeans':[True,{'n_clusters':['auto']}]} #Clustering
