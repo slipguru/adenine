04985f1b7152990a37e5f63b1f1bfb53862e6417
Samuele Fiorini <samuele.fiorini@gmail.com>
2017-05-16 11:01:46 +0200

    requirements updated

--- setup.py
+++ setup.py
@@ -39,7 +39,9 @@ setup(
                       'scikit-learn (>=0.18)',
                       'matplotlib (>=1.5.1)',
                       'seaborn (>=0.7.0)',
-                      'joblib',
-                      'fastcluster (>=1.1.20)'],
+                    #   'joblib',
+                      'fastcluster (>=1.1.20)',
+                      'GEOparse (>=0.1.10)',
+                      'pydot (>=1.2.3)'],
     scripts=['scripts/ade_run.py', 'scripts/ade_analysis.py'],
 )
